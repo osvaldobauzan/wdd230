cf280f4ca616d07908b616bd16bf7c8287ebc45c
osvaldobauzan <osvaldo.bauzan@gmail.com>
2022-03-03 14:16:56 -0600

    profetas

--- lesson-9/JS/prophets.js
+++ lesson-9/JS/prophets.js
@@ -6,14 +6,12 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
-  });
+    const prophets = jsonObject['prophets'];
+    prophets.forEach(displayProphets);
+});
 
-  const prophets = jsonObject['prophets'];
 
-  prophets.forEach(displayProphets);
-
-  function displayProphets(prophet) {
+function displayProphets(prophet) {
     // Create elements to add to the document
     let card = document.createElement('section');
     let h2 = document.createElement('h2');
@@ -21,7 +19,10 @@ fetch(requestURL)
   
     // Change the textContent property of the h2 element to contain the prophet's full name
     h2.textContent = prophet.name + ' ' + prophet.lastname;
-  
+    
+
+
+ 
     // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
     portrait.setAttribute('src', prophet.imageurl);
     portrait.setAttribute('alt', 'Portait of ' + prophet.name + ' ' + prophet.lastname);
@@ -30,6 +31,7 @@ fetch(requestURL)
     // Add/append the section(card) with the h2 element
     card.appendChild(h2);
     card.appendChild(portrait);
+    
   
     // Add/append the existing HTML div with the cards class with the section(card)
     cards.appendChild(card);
